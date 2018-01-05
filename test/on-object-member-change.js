e893f2692d9642866e6c51d228e72990d519895d
Dennis Weiershaeuser <dennis.weiershaeuser@informatik.hs-fulda.de>
2018-01-05 07:13:38 +0100

    test for new feature

--- test/on-object-member-change.js
+++ test/on-object-member-change.js
@@ -8,3 +8,20 @@ import $ from "jquery"
 
 // actual imports
 import Observable from "../build/Observable"
+
+let o = new Observable({
+    bounds: {
+        left: 0,
+        right: 1,
+        top: 0,
+        bottom: 0
+    },
+    points: [{ x:0, y:0 }, { x:1, y:1 }]
+})
+// new API?
+o.update(o.bounds.left, 100)
+/**
+ * to be able to reserve all namespace for an observable object,
+ * - prefix all observable attributes with '_'.
+ * - or put them in a object named 'observable'.
+ */
